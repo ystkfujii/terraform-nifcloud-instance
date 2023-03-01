4a9bcedef06f2551e579ea80814d2e9a96ab3659
Yoshitaka Fujii <76274657+ystkfujii@users.noreply.github.com>
2023-03-01 16:12:41 +0000

    feat: add hooks of pre-commit-terraform

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">=1.3.7"
   required_providers {
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+
     nifcloud = {
       source  = "nifcloud/nifcloud"
       version = ">= 1.7.0, < 2.0.0"
